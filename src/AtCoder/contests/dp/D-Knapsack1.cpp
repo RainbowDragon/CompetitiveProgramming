8c0285dce388eeff9a399ef677181196287db38d
Xiang Xuan <xuanxiang@hotmail.com>
2022-08-09 16:38:47 -0700

    AtCoder Educational DP Contest - D. Knapsack 1

--- src/AtCoder/contests/dp/D-Knapsack1.cpp
+++ src/AtCoder/contests/dp/D-Knapsack1.cpp
@@ -14,26 +14,29 @@ int main()
     int N, W;
     cin >> N >> W;
     int weight[N], value[N];
-    ll dp[N][W+1];
+    ll dp[W+1];
 
     for (int i = 0; i < N; i++)
     {
         cin >> weight[i] >> value[i];
     }
 
+    for (int i = 0; i <= W; i++)
+    {
+        dp[i] = 0;
+    }
+
     for (int i = 0; i < N; i++)
     {
-        dp[i][0] = 0;
-        for (int j = 1; j <= W; j++)
+        for (int j = W; j > 0; j--)
         {
-            dp[i][j] = dp[i-1][j];
             if (j >= weight[i]) {
-                dp[i][j] = max(dp[i][j], dp[i-1][j-weight[i]]+value[i]);
+                dp[j] = max(dp[j], dp[j-weight[i]]+value[i]);
             }
         }
     }
 
-    cout << dp[N-1][W] << endl;
+    cout << dp[W] << endl;
 
     return 0;
 }
