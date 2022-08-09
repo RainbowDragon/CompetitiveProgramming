2c273acd93db00c9aaf114cd3fe2238358280305
Xiang Xuan <xuanxiang@hotmail.com>
2022-08-09 10:53:55 -0700

    AtCoder Educational DP Contest - C. Vacation

---
+++ src/AtCoder/contests/dp/C-Vacation.cpp
@@ -0,0 +1,36 @@
+/**
+ *      AtCoder Educational DP Contest - C. Vacation
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    int happiness[N][3];
+    int dp[N][3];
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> happiness[i][0] >> happiness[i][1] >> happiness[i][2];
+    }
+
+    dp[0][0] = happiness[0][0];
+    dp[0][1] = happiness[0][1];
+    dp[0][2] = happiness[0][2];
+
+    for (int i = 1; i < N; i++)
+    {
+        dp[i][0] = max(dp[i-1][1], dp[i-1][2]) + happiness[i][0];
+        dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + happiness[i][1];
+        dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + happiness[i][2];
+    }
+
+    cout << max(dp[N-1][0], max(dp[N-1][1], dp[N-1][2])) << endl;
+
+    return 0;
+}
