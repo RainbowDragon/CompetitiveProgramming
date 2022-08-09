d98338a872769a16ab99edaf4944283114d1b9f7
Xiang Xuan <xuanxiang@hotmail.com>
2022-08-09 11:02:51 -0700

    AtCoder Educational DP Contest - H. Grid 1

--- src/AtCoder/contests/dp/H-Grid1.cpp
+++ src/AtCoder/contests/dp/H-Grid1.cpp
@@ -6,45 +6,45 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef long long ll;
 
-const int MOD = 1000000007;
+typedef long long ll;
+const int MOD = 1e9+7;
 
 int main()
 {
-    int h, w;
-    cin >> h >> w;
-    bool grid[h][w];
+    int H, W;
+    cin >> H >> W;
+    bool grid[H][W];
 
-    for (int i = 0; i < h; i++)
+    for (int i = 0; i < H; i++)
     {
         string str;
         cin >> str;
-        for (int j = 0; j < w; j++)
+        for (int j = 0; j < W; j++)
         {
             grid[i][j] = (str[j] == '.');
         } 
     }
 
-    ll dp[h][w];
+    ll dp[H][W];
     dp[0][0] = grid[0][0] ? 1 : 0;
-    for (int i = 1; i < h; i++)
+    for (int i = 1; i < H; i++)
     {
         dp[i][0] = grid[i][0] ? dp[i-1][0] : 0;
     }
-    for (int i = 1; i < w; i++)
+    for (int i = 1; i < W; i++)
     {
         dp[0][i] = grid[0][i] ? dp[0][i-1] : 0;
     }
 
-    for (int i = 1; i < h; i++)
-        for (int j = 1; j < w; j++)
+    for (int i = 1; i < H; i++)
+        for (int j = 1; j < W; j++)
         { 
             dp[i][j] = grid[i][j] ? dp[i-1][j]+dp[i][j-1] : 0;
             dp[i][j] %= MOD;
         }     
 
-    cout << dp[h-1][w-1] << endl;
+    cout << dp[H-1][W-1] << endl;
 
     return 0;
 }
