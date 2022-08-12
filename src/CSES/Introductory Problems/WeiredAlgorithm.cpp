3935e35e6a399aed80688c00de78fca8af746c30
Xiang Xuan <xuanxiang@hotmail.com>
2022-08-11 17:57:13 -0700

    CSES Problem Set - Weird Algorithm

---
+++ src/CSES/Introductory Problems/WeiredAlgorithm.cpp
@@ -0,0 +1,29 @@
+/**
+ *      CSES Problem Set - Weird Algorithm
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long n;
+    cin >> n;
+    
+    while (n != 1)
+    {
+        cout << n << " ";
+ 
+        if (n % 2 == 0) {
+            n = n / 2;
+        }
+        else {
+            n = 3*n + 1;
+        }
+    }
+    cout << 1 << endl;
+
+    return 0;
+}
