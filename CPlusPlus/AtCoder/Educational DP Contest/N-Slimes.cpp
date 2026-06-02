486378836c6cc2d586cfd5bdbaf3d9057d3cfc50
Xiang Xuan <xuanxiang@hotmail.com>
2026-06-02 07:33:15 -0700

    AtCoder Educational DP Contest - N. Slimes

--- CPlusPlus/AtCoder/Educational DP Contest/N-Slimes.cpp
+++ CPlusPlus/AtCoder/Educational DP Contest/N-Slimes.cpp
@@ -3,47 +3,60 @@
  *
  */
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 typedef long long ll;
 
+// Use a safe infinity that won't clip maximum possible combinations
+const ll INF = 1e18; 
+
 int main()
 {
+    // Optimize standard I/O operations for competitive programming
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int N;
-    cin >> N;
+    if (!(cin >> N)) return 0;
 
-    int a[N+1];
+    // Build the prefix sum array directly on the fly
+    vector<ll> prefixSum(N + 1, 0);
     for (int i = 1; i <= N; i++)
     {
-        cin >> a[i];
+        ll val;
+        cin >> val;
+        prefixSum[i] = prefixSum[i - 1] + val;
     }
 
-    ll prefixSum[N+1];
-    prefixSum[0] = 0;
-    for (int i = 1; i <= N; i++)
-    {
-        prefixSum[i] = prefixSum[i-1] + a[i];
-    }
+    // Allocate DP matrix safely on the heap
+    // dp[i][j] stores the minimum cost to merge candies from index i to j
+    vector<vector<ll>> dp(N + 1, vector<ll>(N + 1, 0));
 
-    ll dp[N+1][N+1];
-    memset(dp, 0, sizeof dp);
-
-    for (int j = 1; j < N; j++)
+    // 'len' represents the length of the interval we are merging (from 2 up to N)
+    for (int len = 2; len <= N; len++)
     {
-        for (int i = 1; i+j <= N; i++)
+        for (int i = 1; i <= N - len + 1; i++)
         {
-            dp[i][i+j] = 1e13;
-            for (int k = i; k < i+j; k++)
+            int j = i + len - 1; // End index of the interval
+            
+            dp[i][j] = INF;
+
+            // Try every possible split point 'k' between i and j-1
+            for (int k = i; k < j; k++)
             {
-                dp[i][i+j] = min(dp[i][i+j], dp[i][k]+dp[k+1][i+j]);
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j]);
             }
-            dp[i][i+j] += (prefixSum[i+j] - prefixSum[i-1]);
+
+            // Add the cost of the final merge step for the entire range [i, j]
+            dp[i][j] += (prefixSum[j] - prefixSum[i - 1]);
         }
     }
 
-    cout << dp[1][N] << endl;
+    cout << dp[1][N] << "\n";
 
     return 0;
 }
