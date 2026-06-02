9c42c55d75eb6f20c0b49dc66bb124dbf60a34e8
Xiang Xuan <xuanxiang@hotmail.com>
2026-06-02 07:31:26 -0700

    AtCoder Educational DP Contest - L. Deque

--- CPlusPlus/AtCoder/Educational DP Contest/L-Deque.cpp
+++ CPlusPlus/AtCoder/Educational DP Contest/L-Deque.cpp
@@ -3,7 +3,9 @@
  *
  */
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,32 +13,43 @@ typedef long long ll;
 
 int main()
 {
-    int N;
-    cin >> N;
+    // Optimize standard I/O operations for competitive programming
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int a[N];
+    int N;
+    if (!(cin >> N)) return 0;
+
+    vector<ll> a(N);
     for (int i = 0; i < N; i++)
     {
         cin >> a[i];
     }
 
-    ll dp[N][N];
-    memset(dp, 0, sizeof dp);
-
+    // dp[i] will dynamically represent the maximum relative score for an interval starting at 'i'
+    // Base case: Intervals of length 1 (j = 0). The only choice is to take the single element.
+    vector<ll> dp(N);
     for (int i = 0; i < N; i++)
     {
-        dp[i][i] = a[i];
+        dp[i] = a[i];
     }
 
-    for (int j = 1; j < N; j++)
+    // Iterate by interval length (len goes from 2 up to N)
+    for (int len = 2; len <= N; len++)
     {
-        for (int i = 0; i+j < N; i++)
+        // Calculate DP values for intervals of the current length
+        for (int i = 0; i <= N - len; i++)
         {
-            dp[i][i+j] = max(a[i]-dp[i+1][i+j], a[i+j]-dp[i][i+j-1]);
+            int j = i + len - 1; // End index of the interval
+
+            // dp[i+1] currently holds the value for interval [i+1, j] (from length len-1)
+            // dp[i] currently holds the value for interval [i, j-1] (from length len-1)
+            dp[i] = max(a[i] - dp[i + 1], a[j] - dp[i]);
         }
     }
 
-    cout << dp[0][N-1] << endl;
+    // The answer for the full deck spanning from index 0 to N-1
+    cout << dp[0] << "\n";
 
     return 0;
 }
