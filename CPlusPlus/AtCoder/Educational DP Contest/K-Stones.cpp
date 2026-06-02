8055620917d1197b2e59fe4fffeff24bc0d89e2a
Xiang Xuan <xuanxiang@hotmail.com>
2026-06-02 07:28:43 -0700

    AtCoder Educational DP Contest - K. Stones

--- CPlusPlus/AtCoder/Educational DP Contest/K-Stones.cpp
+++ CPlusPlus/AtCoder/Educational DP Contest/K-Stones.cpp
@@ -3,35 +3,53 @@
  *
  */
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    int N, K;
-    cin >> N >> K;
+    // Optimize standard I/O operations for competitive programming
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int a[N];
+    int N, K;
+    if (!(cin >> N >> K)) return 0;
+
+    vector<int> a(N);
     for (int i = 0; i < N; i++)
     {
         cin >> a[i];
     }
 
-    bool dp[K+1];
-    memset(dp, false, sizeof dp);
+    // Sorting allows us to break early in the inner loop
+    sort(a.begin(), a.end());
+
+    // vector<bool> is space-optimized by the STL (uses 1 bit per boolean)
+    // and automatically initializes everything to false.
+    vector<bool> dp(K + 1, false);
 
     for (int i = 1; i <= K; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            if (a[j] <= i && !dp[i-a[j]]) {
+            // Optimization: Since 'a' is sorted, if a[j] > i, all subsequent moves will also be too large
+            if (a[j] > i) {
+                break;
+            }
+
+            // If making a move of size a[j] leaves the opponent in a losing state, 
+            // then the current state 'i' is a winning state.
+            if (!dp[i - a[j]]) {
                 dp[i] = true;
+                break; // Optimization: Once 'i' is proven to be a winning state, skip remaining moves
             }
         }
     }
 
-    cout << (dp[K] ? "First" : "Second") << endl;
+    cout << (dp[K] ? "First" : "Second") << "\n";
 
     return 0;
 }
