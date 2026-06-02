35ca278ba02262f37d2e6893de68f9561fb598a7
Xiang Xuan <xuanxiang@hotmail.com>
2026-06-02 07:01:04 -0700

    AtCoder Educational DP Contest - C. Vacation

--- CPlusPlus/AtCoder/Educational DP Contest/C-Vacation.cpp
+++ CPlusPlus/AtCoder/Educational DP Contest/C-Vacation.cpp
@@ -3,34 +3,44 @@
  *
  */
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
+    // Optimize standard I/O operations for competitive programming
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int N;
-    cin >> N;
-    int happiness[N][3];
-    int dp[N][3];
+    if (!(cin >> N)) return 0;
 
-    for (int i = 0; i < N; i++)
-    {
-        cin >> happiness[i][0] >> happiness[i][1] >> happiness[i][2];
-    }
+    // We only need to store the DP results of the "previous day"
+    // Initialize for Day 1 (index 0)
+    int prev_A, prev_B, prev_C;
+    cin >> prev_A >> prev_B >> prev_C;
 
-    dp[0][0] = happiness[0][0];
-    dp[0][1] = happiness[0][1];
-    dp[0][2] = happiness[0][2];
-
+    // Process Days 2 to N dynamically
     for (int i = 1; i < N; i++)
     {
-        dp[i][0] = max(dp[i-1][1], dp[i-1][2]) + happiness[i][0];
-        dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + happiness[i][1];
-        dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + happiness[i][2];
+        int curr_A, curr_B, curr_C;
+        cin >> curr_A >> curr_B >> curr_C;
+
+        // Calculate maximums for the current day using the previous day's data
+        int next_A = max(prev_B, prev_C) + curr_A;
+        int next_B = max(prev_A, prev_C) + curr_B;
+        int next_C = max(prev_A, prev_B) + curr_C;
+
+        // Move forward to the next day
+        prev_A = next_A;
+        prev_B = next_B;
+        prev_C = next_C;
     }
 
-    cout << max(dp[N-1][0], max(dp[N-1][1], dp[N-1][2])) << endl;
+    // The answer is the maximum value possible on the final day
+    cout << max({prev_A, prev_B, prev_C}) << "\n";
 
     return 0;
 }
