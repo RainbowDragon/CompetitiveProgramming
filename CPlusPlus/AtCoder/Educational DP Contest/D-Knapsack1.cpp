b08206e75dc78c97f624d8dab2c6d61220454ad3
Xiang Xuan <xuanxiang@hotmail.com>
2026-06-07 11:11:26 -0700

    AtCoder Educational DP Contest - D. Knapsack 1

--- CPlusPlus/AtCoder/Educational DP Contest/D-Knapsack1.cpp
+++ CPlusPlus/AtCoder/Educational DP Contest/D-Knapsack1.cpp
@@ -20,6 +20,7 @@ int main()
     int N, W;
     if (!(cin >> N >> W)) return 0;
 
+    // dp[j] stores the maximum value under the weight 'j'
     vector<ll> dp(W + 1, 0);
 
     // Process each item on the fly without storing them in arrays
