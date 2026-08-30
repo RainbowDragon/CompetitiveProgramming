813cc9d340c2e6096114a900031c834a6be5b186
Xiang Xuan <xuanxiang@hotmail.com>
2026-08-29 22:20:26 -0700

    CSES Problem Set - Dynamic Programming - Array Description

---
+++ CPlusPlus/CSES/Dynamic Programming/ArrayDescription.cpp
@@ -0,0 +1,83 @@
+/**
+ *      CSES Problem Set - Dynamic Programming - Array Description
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+typedef long long ll;
+const int MOD = 1e9+7;
+ 
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+ 
+    vector<int> x(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x[i];
+    }
+ 
+    vector<ll> dp(m+2, 0);
+    if (x[0] != 0) 
+    {
+        dp[x[0]] = 1;
+    }
+    else 
+    {
+        for (int i = 1; i <= m; i++)
+        {
+            dp[i] = 1;
+        }
+    }
+ 
+    vector<ll> temp(m+2, 0);
+
+    for (int i = 1; i < n; i++)
+    {
+        int value = x[i];
+        if (value != 0) 
+        {
+            dp[value] += dp[value-1];
+            dp[value] += dp[value+1];
+            dp[value] %= MOD;
+            value = dp[value];
+ 
+            for (int j = 1; j <= m; j++)
+            {
+                dp[j] = 0;
+            }
+ 
+            dp[x[i]] = value;
+        }
+        else 
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                temp[j] = dp[j];
+                temp[j] += dp[j-1];
+                temp[j] += dp[j+1];
+                temp[j] %= MOD;
+            }
+            
+            for (int j = 1; j <= m; j++)
+            {
+                dp[j] = temp[j];
+            }
+        }
+    }
+ 
+    ll result = 0;
+    for (int i = 1; i <= m; i++)
+    {
+        result += dp[i];
+        result %= MOD;
+    }
+ 
+    cout << result << endl;
+ 
+    return 0;
+}
