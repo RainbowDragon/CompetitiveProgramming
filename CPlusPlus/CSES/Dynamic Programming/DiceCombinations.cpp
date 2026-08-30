8c9e79fbf22bfaee6933ade59ede41875126d5f1
Xiang Xuan <xuanxiang@hotmail.com>
2026-08-29 21:54:28 -0700

    CSES Problem Set - Dynamic Programming - Dice Combinations

--- CPlusPlus/CSES/Dynamic Programming/DiceCombinations.cpp
+++ CPlusPlus/CSES/Dynamic Programming/DiceCombinations.cpp
@@ -15,15 +15,15 @@ int main()
     int n;
     cin >> n;
     
-    ll dp[n+1];
+    vector<ll> dp(n+1, 0);
     dp[0] = 1;
  
     for (int i = 1; i <= n; i++)
     {
-        dp[i] = 0;
         for (int d = 1; d <= 6; d++)
         {
-            if (i >= d) {
+            if (i >= d) 
+            {
                 dp[i] += dp[i-d];
                 dp[i] %= MOD;
             }
