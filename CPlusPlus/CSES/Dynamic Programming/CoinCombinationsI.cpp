e34fc5e24c4abdfdc8a2dc9e6cede3cc8b676224
Xiang Xuan <xuanxiang@hotmail.com>
2026-08-29 23:47:41 -0700

    CSES Problem Set - Dynamic Programming - Coin Combinations I

--- CPlusPlus/CSES/Dynamic Programming/CoinCombinationsI.cpp
+++ CPlusPlus/CSES/Dynamic Programming/CoinCombinationsI.cpp
@@ -6,8 +6,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
- 
-typedef long long ll;
+
 const int MOD = 1e9+7;
  
 int main()
@@ -21,7 +20,7 @@ int main()
         cin >> coin[i];
     }
  
-    vector<ll> dp(x+1, 0);
+    vector<int> dp(x+1, 0);
     dp[0] = 1;
 
     for (int i = 1; i <= x; i++)
