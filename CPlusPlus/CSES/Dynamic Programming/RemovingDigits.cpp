060adc0a107e82ba514a6e65f4064220754630c9
Xiang Xuan <xuanxiang@hotmail.com>
2026-08-29 22:10:09 -0700

    CSES Problem Set - Dynamic Programming - Removing Digits

--- CPlusPlus/CSES/Dynamic Programming/RemovingDigits.cpp
+++ CPlusPlus/CSES/Dynamic Programming/RemovingDigits.cpp
@@ -12,17 +12,20 @@ int main()
     int n;
     cin >> n;
     
-    int dp[n+1];
+    vector<int> dp(n+1, 1e9);
     dp[0] = 0;
+
     for (int i = 1; i <= n; i++)
     {
-        if (i < 10) {
+        if (i < 10) 
+        {
             dp[i] = 1;
         }
-        else {
-            dp[i] = 1e9;
+        else 
+        {
             int value = i;
-            while (value > 0) {
+            while (value > 0) 
+            {
                 int d = value % 10;
                 dp[i] = min(dp[i], dp[i-d]+1);
                 value /= 10;
