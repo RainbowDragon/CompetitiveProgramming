a6389bc280d1b013743c1f5c95e81e7bf790b2f0
Xiang Xuan <xuanxiang@hotmail.com>
2026-08-29 21:57:15 -0700

    CSES Problem Set - Dynamic Programming - Minimizing Coins

--- CPlusPlus/CSES/Dynamic Programming/MinimizingCoins.cpp
+++ CPlusPlus/CSES/Dynamic Programming/MinimizingCoins.cpp
@@ -12,26 +12,27 @@ int main()
     int n, x;
     cin >> n >> x;
     
-    int coin[n];
+    vector<int> coin(n);
     for (int i = 0; i < n; i++)
     {
         cin >> coin[i];
     }
  
-    int dp[x+1];
+    vector<int> dp(x+1, 1e9);
     dp[0] = 0;
     for (int i = 1; i <= x; i++)
     {
-        dp[i] = 1e9;
         for (int c : coin)
         {
-            if (i >= c) {
+            if (i >= c) 
+            {
                 dp[i] = min(dp[i], dp[i-c]+1);
             }
         }
     }
  
-    if (dp[x] == 1e9) {
+    if (dp[x] == 1e9) 
+    {
         dp[x] = -1;
     }
     
